2e8928c1f7ca3acff188ed2042384fbd6782f589
Heric Macedo Oliveira Silva <heric_dev@hotmail.com>
2021-09-20 17:20:55 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 
-function App(): JSX.Element {
+export default function App(): JSX.Element {
     return (
         <div className="App">
             <h1>Ola mundo!</h1>
         </div>
     );
 }
-
-export default App;
