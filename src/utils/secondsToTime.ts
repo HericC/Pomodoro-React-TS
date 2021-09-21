e157f5a3392489b2036eb1c40448f5774b28d9d1
Heric Macedo Oliveira Silva <heric_dev@hotmail.com>
2021-09-21 01:10:03 -0300

    upgrade util: seconds to time

--- src/utils/secondsToTime.ts
+++ src/utils/secondsToTime.ts
@@ -1,6 +1,4 @@
-export default function secondsToTime(seconds: number): string {
-    return new Date(seconds * 1000)
-        .toISOString()
-        .substr(11, 8)
-        .replace(/^(00:){2}|^(00:){1}/, '');
+export default function secondsToTime(seconds: number, full = false): string {
+    const time = new Date(seconds * 1000).toISOString().substr(11, 8);
+    return full ? time : time.replace(/^(00:){2}|^(00:){1}/, '');
 }
