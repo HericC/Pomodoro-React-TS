451df231d20f72cdb82b13e8f0966b217d06714b
Heric Macedo Oliveira Silva <heric_dev@hotmail.com>
2021-09-21 00:55:16 -0300

    component template

---
+++ src/components/template/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { DivStyled } from './styled';
+
+export default function Template(): JSX.Element {
+    return (
+        <DivStyled>
+            <h2>Template</h2>
+        </DivStyled>
+    );
+}
